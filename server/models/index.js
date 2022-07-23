bfe22ef025fd72cd9d4bb255cd54329325bf7fbd
Tanya Jurecki <tljurecki@gmail.com>
2022-07-23 12:13:18 -0400

    added mongoose and user model

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Comments = require('./Comments');
+const blogPost = require('./blogPost');
+
+module.exports = { User, Comments, blogPost };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,7 @@
+//import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+//create typeDefs
+const typeDefs = gql ``;
+
+module.exports = typeDefs;
