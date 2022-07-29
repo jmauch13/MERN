09f87aa123aad94a5132c691b7bd45d9432b44ef
Tanya Jurecki <tljurecki@gmail.com>
2022-07-29 14:45:52 -0400

    changed layout

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,25 +13,9 @@ type User {
     internPosts: [InternPost]
     friends: [User]
 }
-type EducationPost {
-    _id: ID
-    educationText: String
-    createdAt: String
-    username: String
-    commentCount: String
-    comments: [Comments]
-}
 type JobPost {
     _id: ID
     jobText: String
-    createdAt: String
-    username: String
-    commentCount: String
-    comments: [Comments]
-}
-type InternPost {
-    _id: ID
-    internText: String
     createdAt: String
     username: String
     commentCount: String
@@ -54,23 +38,15 @@ type Query {
     me: User
     users: [User]
     user(username: String!): User
-    educationPosts(username: String): [EducationPost]
-    educationPost(_id: ID!): EducationPost
     jobPosts(username: String): [JobPost]
     jobPost(_id: ID!): JobPost
-    internPosts(username: String): [InternPost]
-    internPost(_id: ID!): InternPost
 }
 
 type Mutation {
     login(email: String!, password: String!): Auth 
     addUser(username: String!, email: String!, password: String!): Auth
-    addEducationPost(educationtText: String!): EducationPost 
-    addEducationComment(educationPostId: ID!, commentBody: String!): EducationPost
     addJobPost(jobText:String!): JobPost
-    addJobComment(jobTextId: ID!, commentBody: String!): JobPost
-    addInternPost(internText: String!): InternPost
-    addInternComment(internTextId: ID!, commentBody: String!): InternPost
+    addComment(jobTextId: ID!, commentBody: String!): JobPost 
     addFriend(friendId: ID!): User
 }
 `;
